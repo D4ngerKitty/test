e4070545573b92a0d8a01daa2201f6394c8d20d6
LucasMayhew <59377840+LucasMayhew@users.noreply.github.com>
2022-05-31 10:58:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-
-        const ws = new WebSocket("https://arcade.makecode.com/")
-        ws.onerror = () => console.log("error")
-        ws.onmessage = (msg) => {
-            const data = msg.data;
-            console.log(`--> ${data}`)
-        }
-        ws.onopen = () => {
-            forever(() => {
-                const m = `makecode ${control.millis()}`;
-                console.log(`<-- ${m}`)
-                ws.send(m);
-                pause(1000)
-            })
-        }
+// string tests
+const ws = new WebSocket("wss://echo.websocket.org")
+ws.onerror = () => console.log("error")
+ws.onmessage = (msg) => {
+    const data = msg.data;
+    console.log(`--> ${data}`)
+}
+ws.onopen = () => {
+    forever(() => {
+        const m = `makecode ${control.millis()}`;
+        console.log(`<-- ${m}`)
+        ws.send(m);
+        pause(1000)
+    })
+}
